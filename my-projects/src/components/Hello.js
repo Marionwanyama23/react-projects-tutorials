3d1994c81c7f77fb8307dc5b4f95392b6678b7a7
+ <+>
2023-07-25 20:15:45 +0300

    commit

--- my-projects/src/components/Hello.js
+++ my-projects/src/components/Hello.js
@@ -1,5 +1,12 @@
+/*
 function Hello() {
     return <h1>Hello World!</h1>
+}
+*/
+//FUNCTION COMPONENT
+const name = "Marion Wanyama"
+let Hello = () => {
+return <h1>Hello World! {name}</h1>
 }
 
 export default Hello;
---
+++ my-projects/src/components/Message.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react"
+//CLASS COMPONENT
+//class components inherit from react components
+//uses render() and return
+
+class Message extends Component {
+    render() {
+        return <h2>This is a class component</h2>;
+    }
+}
+ export default Message;
