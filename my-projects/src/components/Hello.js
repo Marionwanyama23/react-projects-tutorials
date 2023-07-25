613d7826115e378d35bf62f41c176cffb0d29626
+ <+>
2023-07-25 20:45:57 +0300

    commit

--- my-projects/src/components/Hello.js
+++ my-projects/src/components/Hello.js
@@ -6,7 +6,11 @@ function Hello() {
 //FUNCTION COMPONENT
 const name = "Marion Wanyama"
 let Hello = () => {
-return <h1>Hello World! {name}</h1>
+return <h1>The message is: {displayMessage()}</h1>
+}
+
+const displayMessage = () =>{
+    return 'I need help!'
 }
 
 export default Hello;
