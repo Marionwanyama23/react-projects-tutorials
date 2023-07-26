98c62c1b64d73f04dc86ffdda461935fee442b53
+ <+>
2023-07-26 19:21:36 +0300

    commit

--- my-projects/src/App.js
+++ my-projects/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 //import Hello from './components/Hello'
 //import Message from './components/Message'
 import Profile from './components/Profile'
+import ClassProp from './components/ClassProps';
 
 function App() {
   return (
     <div className="App">
      <Profile name = "Marion"/>
+     <ClassProp message = "This is a prop in a class based component"/>
     </div>
   );
 }
--- my-projects/src/components/ClassProps.js
+++ my-projects/src/components/ClassProps.js
@@ -1,8 +1,9 @@
 import { Component } from "react";
 
+//this keyword is used to accept props
 class ClassProp extends Component {
     render (){
-        return <h3>Props in a class component</h3>
+        return <h3>Message: {this.props.message}</h3>
     }
 }
 
