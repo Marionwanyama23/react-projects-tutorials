f161caa876f043ab874f6b830c9dcf5b51103546
+ <+>
2023-07-26 20:44:22 +0300

    commit

--- my-projects/src/components/ClassProps.js
+++ my-projects/src/components/ClassProps.js
@@ -1,6 +1,8 @@
 import { Component } from "react";
 
 //this keyword is used to accept props
+//rfec- function component
+//rcc- class component
 class ClassProp extends Component {
     render (){
         return <h3>Message: {this.props.message}</h3>
