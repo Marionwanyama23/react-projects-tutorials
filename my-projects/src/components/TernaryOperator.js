43234c012cf7979ff268e6f2970124cf316ffc11
+ <+>
2023-08-01 12:10:58 +0300

    commit

---
+++ my-projects/src/components/TernaryOperator.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+
+function TernaryOperator() {
+    const [display, setDisplay] = useState(true)
+  return display ? (
+    <div>
+        <h2>This statement appears when the state is set to true</h2>
+    </div>
+  ): (
+    <div>
+        <h2>This statement appears when the state is set to false</h2>
+    </div>
+  )
+}
+
+export default TernaryOperator
